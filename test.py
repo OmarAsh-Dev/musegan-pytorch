350dd67c94d42d5ee3e43d10561020fab45136ce
cliffordkleinsr <cnjoroge925@gmail.com>
2023-05-02 08:21:05 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from musegan.trainner import WassersteinLoss,GradientPenalty
 from musegan.archs import TemporalNetwork, BarGenerator, MuseCritic, initialize_weights, MuseGenerator
-from musegan.dataset import LPDDataset
+from musegan.dataset import LPDDataset, binarise_output
 from musegan.trainner import Trainer
